aab4c3ff98c000a9253ae5771595b0248c49ebc1
erebrus <erebrus@gmail.com>
2024-08-18 18:12:42 +0100

    fixed collision warnings

--- godot/src/world/room/hint/world_hint.gd
+++ godot/src/world/room/hint/world_hint.gd
@@ -6,7 +6,5 @@ var data:Hint
 func _on_body_entered(_body: Node2D) -> void:
 	Logger.info("Found hint.")
 	Events.on_hint_found.emit()
-	data.found = true
-	collision_mask=0
-	$CollisionShape2D.disabled = true
+	data.found = true	
 	call_deferred("queue_free")

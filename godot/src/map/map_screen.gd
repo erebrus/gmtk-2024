2cbe19169953d79b3fbf4a94bb589a6174e8581e
Ángela <angela.lbsr@gmail.com>
2024-08-18 18:33:52 +0200

    map score popup

--- godot/src/map/map_screen.gd
+++ godot/src/map/map_screen.gd
@@ -1,5 +1,25 @@
 extends Node
 
+@export_category("Score")
+@export var min_accuracy = {
+	"E"= 0.3,
+	"D"= 0.5,
+	"C"= 0.7,
+	"B"= 0.8,
+	"A"= 0.9,
+	"S"= 0.999,
+}
+
+@export var passing_grade := "C"
+
+@export var bonus_time = {
+	"E"= 0,
+	"D"= 0,
+	"C"= 0,
+	"B"= 0,
+	"A"= 0,
+	"S"= 0,
+}
 
 @export_category("Scenes")
 @export var RoomScene: PackedScene
@@ -65,3 +85,29 @@ func _on_map_changed() -> void:
 	Logger.info("Map changed. Evaluating...")
 	var score = dungeon.evaluate()
 	Logger.info("Score: %s/%s" % [score, target_dungeon.max_score])
+	
+
+func _on_evaluate_button_pressed():
+	%EvaluationPopup.show()
+	var max_score = target_dungeon.max_score
+	var score = dungeon.evaluate()
+	Logger.info("Score: %s/%s" % [score, max_score])
+	
+	var factor = score / max_score
+	var letter = "F"
+	Logger.info("%s" % factor)
+	for letter_score in min_accuracy:
+		if factor > min_accuracy[letter_score]:
+			letter = letter_score
+	
+	%ScoreLabel.text = letter
+	
+
+func _on_continue_button_pressed():
+	# TODO: go to next level
+	Globals.start_game()
+
+
+func _on_retry_button_pressed():
+	# TODO: retry same level
+	Globals.start_game()
